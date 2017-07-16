ed514afd90555cfee998e58bd69698dcc17d0b71
Suresh Marru <smarru@apache.org>
2017-07-16 10:06:35 -0400

    initial commit from a theme given by chet

---
+++ partials/footer.blade.php
@@ -0,0 +1,29 @@
+<footer class="dark-gray-bg" role="contentinfo">
+
+    <div class="footer-wrapper">
+        <section class="contact" itemscope itemtype="http://schema.org/CollegeOrUniversity">
+
+            <a href="mailto:research-computing@siu.eduu" itemprop="email">Webmaster: research-computing@siu.edu</a>
+            </p>
+        </section>
+        <section class="footer-links">
+            <p>©2017 SIU Board of Trustees</p>
+            </p>
+        </section>
+    </div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript">
+</script>
+<script src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/scripts/responsive-nav.js" type="text/javascript">
+</script>
+<script src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/scripts/modernizr.js" type="text/javascript">
+</script>
+<script src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/scripts/gestures.js" type="text/javascript">
+</script>
+<script src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/scripts/respond.min.js" type="text/javascript">
+</script>
+<!--respond.min allows responsive css3 in explorer 8 and below-->
+<script type="text/javascript">
+    var navigation = responsiveNav("#nav-collapse");
+</script>
+<!-- Resource jQuery -->
---
+++ partials/template.blade.php
@@ -0,0 +1,19 @@
+
+    <main role="main" id="maininside">
+        <!--The Title-->
+        <h2 class="bigger">SIU Science Gateway</h2>
+        <div class="section group">
+            <div class="col span_12_of_12 none">
+                <div>
+                    <div class="col span_6_of_12 none"><img src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/images/big-dawg.jpg" /></div>
+                    <div class="col span_6_of_12 white"><p class="bigger">The SIU Science Gateway provides access to SIU's BigDog high performance computing cluster and is part of a network of science gateways used to access numerous state of the art supercomputers at premier universities and research centers. You must have authorization in order to log in to the SIU Science Gateway. For more information, email research-computing@siu.edu or see one of these web sites:</p>
+                        <ul class="navlist-gray-dot">
+                            <li><a href="http://sciencegateways.org/">http://sciencegateways.org/</a></li>
+                            <li><a href="http://oit.siu.edu/rcc/bigdog/">http://oit.siu.edu/rcc/bigdog/</a></li>
+                            <li><a href="https://www.xsede.org/gateways-overview">https://www.xsede.org/gateways-overview</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
